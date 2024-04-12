8323af390db12d24564259efcee9e841004f63a3
sharalencar <155564948+TchibanaMei@users.noreply.github.com>
2024-04-12 19:23:30 -0300

    Projeto concluído

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css";
+
+function Rodape() {
+  return (
+    <footer className={styles.footer}>
+      <h2>Desenvolvido por Alura.</h2>
+    </footer>
+  );
+}
+
+export default Rodape;
---
+++ src/pages/Player/index.js
@@ -0,0 +1,42 @@
+import Banner from "componentes/Banner";
+import styles from "./Player.module.css";
+import Titulo from "componentes/Titulo";
+import { useParams } from "react-router-dom";
+import NaoEncontrada from "pages/NaoEncontrada";
+import { useEffect, useState } from "react";
+
+function Player() {
+  const parametros = useParams();
+  const [video, setVideo] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/TchibanaMei/cinetag-api/videos?id=${parametros.id}`
+    )
+      .then((res) => res.json())
+      .then((dados) => setVideo(...dados));
+  }, []);
+  if (!video) {
+    return <NaoEncontrada />;
+  }
+  return (
+    <>
+      <Banner imagem="player" />
+      <Titulo>
+        <h1>Player</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <iframe
+          width="100%"
+          height="100%"
+          src={video.link}
+          title={video.titulo}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </section>
+    </>
+  );
+}
+
+export default Player;
